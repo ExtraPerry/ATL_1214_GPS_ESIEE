801b8aa48a67ce066b105c94b4dd3d53e8a61c1f
gervaisp <pierre.gervais@edu.esiee.fr>
2022-02-08 15:56:45 +0100

    rsgrz

--- Nav.java
+++ Nav.java
@@ -15,4 +15,46 @@ public class Nav {
             System.out.println(angle + " : " + toRad(angle));
         }
     }
+    
+    /**
+     * fonction getColat() permettant de retourner la colatitude en radians d’une latitude exprimée en degrés
+     */
+    static double getColat(double lat)
+    {
+        return (double)((Math.PI / 2) - Math.toRadians(lat));
+    }
+    
+    /**
+     * Turns DMS to DD
+     */
+    static double DMSToDD(int[] dms)
+    {
+        return (double)(dms[0] + dms[1] / 60 + dms[2] / 3600);
+    }
+    
+    /**
+     * Turns DD to DMS
+     */
+    static int[] DDToDMS(double latlon)
+    {
+        int[] vDms = new int[3];
+        
+        vDms[0] = (int)latlon;
+        latlon = (latlon - vDms[0]) * 60;
+        vDms[1] = (int)latlon;
+        latlon = (latlon - vDms[1]) * 60;
+        vDms[2] = (int)latlon;
+        
+        return vDms;
+    }
+    
+    /**
+     * 
+     */
+    static double getOrthodromicDistance(double[] A, double[] B)
+    {
+        double vPerimetreTerre = Math.PI * 6371 * 2; //Perimetre en km.
+        
+        return 0;
+    }
 }
